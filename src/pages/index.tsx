4054e4af96757c4a5797178ba22be9c6e12a84a9
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-14 20:51:34 -0300

    preparando formulário de pesquisa

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
+import { FormEvent, useState } from "react";
+
 export default function Home(): JSX.Element {
+  const [search, setSearch] = useState('');
+
+  function handleSearch(e: FormEvent) {
+    e.preventDefault();
+
+  }
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Search</h1>
+
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={search}
+          onChange={e => setSearch(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </form>
+    </div>
   )
 }
