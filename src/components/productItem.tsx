0ed2205f6a0ee49a1853298fab4f4da33a04da34
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-15 10:18:55 -0300

    determinando método de comparação do Memo para propriedades como objetos

--- src/components/productItem.tsx
+++ src/components/productItem.tsx
@@ -16,4 +16,15 @@ function ProductItemComponent({ product }: ProductItemProps) {
   )
 }
 
-export const ProductItem = memo(ProductItemComponent);
+// Shallow compara -> comparação rasa
+// {} === {} // false, realiza comparação de igualdade referencial
+// verifica se estão na mesma posição na memória
+// quando não for indicado realizar comparação com === utilizar 
+// segundo parâmetro do Memo
+
+export const ProductItem = memo(ProductItemComponent,
+  (previousProps, nextProps) => {
+    // retorno para identificar se houveram alterações
+    return Object.is(previousProps.product, nextProps.product);
+  }
+);
