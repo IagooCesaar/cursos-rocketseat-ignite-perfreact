7ae722c517456a1eac93965cb50717eeb5bb521b
Iago César F. Nogueira <iagocesar.sgs@gmail.com>
2021-06-15 10:45:26 -0300

    função para calcular preço total (lenta)

--- src/components/searchResults.tsx
+++ src/components/searchResults.tsx
@@ -10,8 +10,13 @@ interface SearchResultsProps {
 
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = results.reduce((acc, product) => {
+    return acc + product.price;
+  }, 0);
+
   return (
     <div>
+      <h2>Total: {totalPrice}</h2>
       {results.map(product => {
         return (
           <ProductItem product={product} />
